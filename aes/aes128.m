fb64e0fd041ad0e9262db4ca7af8db4a600d7fc8
Aswini Vinay Soreng <aswin.soreng@gmail.com>
2020-08-12 12:23:20 +0530

    changes made

--- aes/aes128.m
+++ aes/aes128.m
@@ -1,12 +1,12 @@
 clc;
 clear all;
 % plaintexthex of 16 bytes of data
-plaintexthex=["00";"11";"22";"33";"44";"55";"66";"77";"88";"99";"aa";"bb";"cc";"dd";"ee";"ff"];
+plaintexthex= ["00";"11";"22";"33";"44";"55";"66";"77";"88";"99";"aa";"bb";"cc";"dd";"ee";"ff"];
 plaintext=hex2dec(plaintexthex); % plaintext in decimal
 
 %key scheduling
 
-keyhex=["00";"31";"22";"33";"44";"55";"66";"77";"88";"99";"aa";"cb";"cf";"ad";"ee";"0f"];
+keyhex= ["00";"e3";"22";"ff";"34";"15";"26";"97";"A8";"B9";"eA";"0b";"0c";"0d";"0e";"0f"];
 key=hex2dec(keyhex);
 rndkeys128=zeros(11,16); % generate matix of 176 to store keys for each round
 rndkeys128(1,:)=key; %% key generation in Row Major Order initial key being the first row
@@ -18,10 +18,10 @@ endfor
 addRoundKey=bitxor(plaintext,key);
 
 % 9 rounds for AES
-for r=1:1
+for r=1:9
   substititeBytes=sbox(addRoundKey); % data present in a column
   shiftRowsResult=shiftrows(substititeBytes'); %% take data in 1 row 
-  mixCol=mixcolumns(shiftRowsResult)
+  mixCol=mixcolumns(shiftRowsResult);
   newKey=rndkeys128(r+1,:);
   addRoundKey=bitxor(newKey,mixCol);
 endfor
@@ -30,7 +30,7 @@ endfor
   substititeBytes=sbox(addRoundKey);
   shiftRowsResult=shiftrows(substititeBytes');
   %mixCol=mixcolumns(shiftRowsResult);
-  newKey=rndkeys128(r+1,:);
+  newKey=rndkeys128(11,:);
   addRoundKey=bitxor(newKey,shiftRowsResult);
   
   %%% Encrypted Text
